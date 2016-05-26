fc4219b6c36382738b9ea9dd6f875305ff8cff67
Rodion Zinovyev <rodionz@012.net.il>
2016-05-26 19:07:25 +0300

    Spep2

---
+++ UserInterface/SignIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+ namespace UserInterface
+{
+    public partial class SignIn : Form
+    {
+        public SignIn()
+        {
+
+            InitializeComponent();
+        }
+
+     
+
+        private void SignIn_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ValidationBooleans.SignInisValid == false)
+            {
+
+                e.Cancel = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

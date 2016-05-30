8b307da63768729304fdca51f36010060837a440
Rodion Zinovyev <rodionz@012.net.il>
2016-05-30 14:53:57 +0300

    Работаем с цветами

---
+++ CommonTypes/MessageData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CommonTypes
+{
+    class MessageData
+    {
+        public string Textmessage
+        { get; set; }
+
+        public DateTime Dt
+        { get; set; }
+
+        public Color Messagecolor
+        { get; set; }
+
+        public UserData Ud
+        { get; set; }
+    }
+}
---
+++ CommonTypes/UserData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CommonTypes
+{
+    public class UserData
+    {
+
+        public string Username
+        { get; set; }
+
+       public Color Usercolor
+        { get; set; }
+
+        public int Userid
+        { get; set; }
+
+       
+
+    }
+}
